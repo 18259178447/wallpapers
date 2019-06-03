6c6f39b8d8008d2c62cd325f72a5e69ef5cb9385
18259178447 <18259178447@sina.cn>
2019-06-03 23:11:27 +0800

    超清版本

--- newWallPaper/utils/config.js
+++ newWallPaper/utils/config.js
@@ -1,12 +1,20 @@
 // 配置文件
-var qudaoIndex = 0;
+var qudaoIndex = 1;
 var qudao = [
   {
     name: "高清壁纸Pro",
     adid: "adunit-84f6d48d83a20427",
-    appid: "awx2d3f209d87668933",
+    appid: "wx2d3f209d87668933",
     dev: "dev-ltqye",
     prod: "prod-n4ws2",
+    use: "dev"
+  },
+  {
+    name: "超清壁纸精选",
+    adid: "adunit-923e33e5e984b835",
+    appid: "wxd3fab7dc660d0a6f",
+    dev: "dev-5f7py",
+    prod: "prod-kizl9",
     use: "dev"
   },
 ]
